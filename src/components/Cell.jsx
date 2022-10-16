1ac13eec344dd6758b6f3088510030ebec48d891
prtvi <prithvippv25@gmail.com>
2022-10-16 19:41:29 +0530

    make grid responsive for different screen sizes

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -8,30 +8,48 @@ const colorOpen = '#e8e8e8';
 const colorTempPath = '#3cc4fe';
 const colorPath = '#3ccf4e';
 
-function getStyle(state) {
+function getStyle(state, mazeSize) {
+	const windowWidth = window.innerWidth;
+	const windowHeight = window.innerHeight;
+
+	let finalWidthHeight = 0;
+
+	if (windowWidth > windowHeight) 
+		finalWidthHeight = windowHeight / (mazeSize * 2.5);
+	else if (windowWidth <= windowHeight) 
+		finalWidthHeight = windowWidth / (mazeSize * 2.5);
+	
 	if (state === 1)
 		// state close
 		return {
 			backgroundColor: colorClose,
 			// border: `1px solid ${colorClose}`,
+			width: finalWidthHeight,
+			height: finalWidthHeight,
 		};
 	else if (state === 0)
 		// state open
 		return {
 			backgroundColor: colorOpen,
 			// border: `1px solid ${colorOpen}`,
+			width: finalWidthHeight,
+			height: finalWidthHeight,
 		};
 	else if (state === 2)
 		// state temp path
 		return {
 			backgroundColor: colorTempPath,
 			border: `1px solid ${colorTempPath}`,
+			width: finalWidthHeight,
+			height: finalWidthHeight,
 		};
 	else if (state === 3)
 		// state path
 		return {
 			backgroundColor: colorPath,
 			border: `1px solid ${colorPath}`,
+			width: finalWidthHeight,
+			height: finalWidthHeight,
 		};
 }
 
@@ -74,7 +92,7 @@ export default function Cell(props) {
 	};
 
 	return (
-		<div className="cell" style={getStyle(state)} onClick={onClickAddBarrier}>
+		<div className="cell" style={getStyle(state, mazeSize)} onClick={onClickAddBarrier}>
 			{getCellDOM(x, y, mazeSize)}
 		</div>
 	);
